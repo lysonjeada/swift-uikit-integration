85cd0388848162324555e885dadcf3fd459b8c1f
lys <lyscalefi@gmail.com>
2024-08-15 22:51:53 -0300

    remove comments and organize files

--- property-app/Home/Property.swift
+++ property-app/Home/Property.swift
@@ -1,19 +1,8 @@
-//
-//  Property.swift
-//  property-app
-//
-//  Created by Amaryllis Rosemaria Baldrez Calefi on 14/08/24.
-//
-
 import SwiftUI
 
 struct Property: Identifiable {
     let id: UUID
-    let type: String // "Apartamento" ou "Casa"
+    let type: String
     let price: String
     let address: String
-}
-
-extension Property {
-    
 }
